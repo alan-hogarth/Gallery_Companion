70676156bce48c259f548c6b32b7228b7d7321be
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-01-09 15:17:58 +0000

    set up of required features in server.js

--- museum_app/server/server.js
+++ museum_app/server/server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
-// changes
+const parser = require('body-parser');
+app.use(parser.json());
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
