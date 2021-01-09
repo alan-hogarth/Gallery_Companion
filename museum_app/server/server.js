ec8af2c5fd859c1799a6971ca01ab60f80a92eaa
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-01-09 15:26:18 +0000

    set up of db connection in server.js

--- museum_app/server/server.js
+++ museum_app/server/server.js
@@ -7,3 +7,13 @@ app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router');
+
+MongoClient.connect("mongodb://localhost:27017")
+    .then((client)=>{
+        const db = client.db("gallery");
+        const objectsCollection = db.collection('objects');
+        const objectsRouter = createRouter(objectsCollection);
+        app.use('/api/objects', objectsRouter);
+    })
+    .catch(console.error);
+    
