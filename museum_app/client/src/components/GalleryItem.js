817ec8a0f96a863f578316597fd56bef3ec71e64
Evan Reid <evan_1995@hotmail.com>
2021-01-10 15:16:37 +0000

    un-commented out lines on GalleryItem so now we can see the images of each item

--- museum_app/client/src/components/GalleryItem.js
+++ museum_app/client/src/components/GalleryItem.js
@@ -12,7 +12,7 @@ const GalleryItem = ({object}) => {
         <div>
             <p>Title: {object.title}</p>
             <p>By: {object.principalOrFirstMaker}</p>
-            {/* <img src={object.webImage["url"]} width="400" height="300" alt={object.title}/> */}
+            <img src={object.webImage["url"]} width="400" height="300" alt={object.title}/>
             <br></br>
             <button onClick={postToFavourites}>Favourite</button>
         </div>
