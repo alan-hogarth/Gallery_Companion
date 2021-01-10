9578a1db093ed3d09544b985bf9a924d69a1ff24
Evan Reid <evan_1995@hotmail.com>
2021-01-10 11:35:58 +0000

    addToFavourites function added to GalleryServices

--- museum_app/client/src/components/GalleryItem.js
+++ museum_app/client/src/components/GalleryItem.js
@@ -1,9 +1,14 @@
 const GalleryItem = ({object}) => {
+
+    
+
     return(
         <div>
             <p>Title: {object.title}</p>
             <p>By: {object.principalOrFirstMaker}</p>
             <img src={object.webImage["url"]} width="400" height="300" alt={object.title}/>
+            <br></br>
+            <button onClick={addToFavourite}>Favorite</button>
         </div>
     )
 };
--- museum_app/client/src/helpers/GalleryService.js
+++ museum_app/client/src/helpers/GalleryService.js
@@ -1,7 +1,19 @@
 const URL = 'https://www.rijksmuseum.nl/api/en/collection/?key=kn4V2P7G&ps=10&toppieces=true&hasImage=true'
+const localURL = 'http://localhost:3000/api/objects/'
 
 export const getAllObjects = ()=>{
     return fetch(URL)
     .then(res => res.json())
 
+};
+
+export const addToFavourite = (data)=>{ 
+    return fetch(localURL, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
 };
