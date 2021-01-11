b7f40dbfcb0c1b62250fc5ce0d5e467c2cf99334
Alan Hogarth <alan.hogarth@outlook.com>
2021-01-11 16:52:57 +0000

    text to speech added to itemDetails.js

--- museum_app/client/src/components/ItemDetails.js
+++ museum_app/client/src/components/ItemDetails.js
@@ -1,7 +1,19 @@
+import { useSpeechSynthesis } from 'react-speech-kit';
+import React, { useState } from 'react';
+
 const ItemDetails = ({itemDetails}) => {
+    
+    const [value, setValue] = useState(itemDetails.artObject.plaqueDescriptionEnglish);
+    const { speak } = useSpeechSynthesis();
+    
     return(
         <>
         <p>{itemDetails.artObject.plaqueDescriptionEnglish}</p>
+        <textarea
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+            />
+            <button onClick={() => speak({ text: value })}>Click for Audio</button>
         </>
     )
 };
