c30c6b1b7b3aa7d7cceb3f63dca87df3520d01af
Alan Hogarth <alan.hogarth@outlook.com>
2021-01-12 20:01:28 +0000

    styling of item details component

--- museum_app/client/src/components/ItemDetails.js
+++ museum_app/client/src/components/ItemDetails.js
@@ -7,46 +7,48 @@ const ItemDetails = ({itemDetails}) => {
     const [value, setValue] = useState(itemDetails.artObject.plaqueDescriptionEnglish);
     const { speak, cancel } = useSpeechSynthesis();
     
-    const style = {
-        container: { },
-        text: { },
-        buttons: { },
-        play: {
-          hover: {
-            backgroundColor: 'GhostWhite'
-          },
-          button: {
-            cursor: 'pointer',
-            pointerEvents: 'none',
-            outline: 'none',
-            backgroundColor: 'Gainsboro',
-            border: 'solid 1px rgba(255,255,255,1)',
-            borderRadius: 6
-          }
-        },
-        pause: {
-          play: { },
-          hover: { }
-        },
-        stop: {
-            play: { 
-                hover: { },
-                button: { }
-            },
-            resume: {
-                play: {
-                    hover: { },
-                    button: { }
-                }
-            }
-        }
-    };
+    // const style = {
+    //     container: { },
+    //     text: { },
+    //     buttons: { },
+    //     play: {
+    //       hover: {
+    //         backgroundColor: 'GhostWhite'
+    //       },
+    //       button: {
+    //         cursor: 'pointer',
+    //         pointerEvents: 'none',
+    //         outline: 'none',
+    //         backgroundColor: 'Gainsboro',
+    //         border: 'solid 1px rgba(255,255,255,1)',
+    //         borderRadius: 6
+    //       }
+    //     },
+    //     pause: {
+    //       play: { },
+    //       hover: { }
+    //     },
+    //     stop: {
+    //         play: { 
+    //             hover: { },
+    //             button: { }
+    //         },
+    //         resume: {
+    //             play: {
+    //                 hover: { },
+    //                 button: { }
+    //             }
+    //         }
+    //     }
+    // };
 
     return(
         <div className="galleryDescription">
-        <img src={itemDetails.artObject.webImage['url']} height="150" width="180"/>
-            <ul>
-                <li>{itemDetails.artObject.longTitle}</li>
+            <h3>{itemDetails.artObject.longTitle}</h3>
+                <figure>
+                <img id="smallImage" src={itemDetails.artObject.webImage['url']} height="150" width="180"/>
+                </figure>
+            <ul> 
                 <li>{itemDetails.artObject.plaqueDescriptionEnglish}</li>
             </ul>
                 <input 
@@ -54,9 +56,9 @@ const ItemDetails = ({itemDetails}) => {
                 value={itemDetails.artObject.plaqueDescriptionEnglish}
                 onChange={(event) => setValue(event.target.value)}
                 />
-                <button onClick={() => speak({ text: itemDetails.artObject.plaqueDescriptionEnglish })}>Click for Audio</button>
+                <button onClick={() => speak({ text: itemDetails.artObject.plaqueDescriptionEnglish })}>▶️ Play Audio</button>
                 <button type="button" onClick={cancel}>
-                    Stop Audio
+                   ⏹ Stop Audio
                 </button> 
 
        </div>
