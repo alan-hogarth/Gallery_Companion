14c8fe602d2c5fe7721637bf7f28aea5c73bc2ef
Evan Reid <evan_1995@hotmail.com>
2021-01-13 16:43:57 +0000

    adding quiz result display correct answers

--- museum_app/client/src/components/QuizCollection.js
+++ museum_app/client/src/components/QuizCollection.js
@@ -5,6 +5,7 @@ const QuizCollection = () => {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
     const [showScore, setShowScore] = useState(false);
+    const [showCorrectAnswers, setShowCorrectAnswers] = useState([]);
 
     const questions = [
         {
@@ -54,23 +55,41 @@ const QuizCollection = () => {
         },
     ];
 
-    const handleAnswerButtonClick = (isCorrect) => {
+
+    const handleAnswerButtonClick = (isCorrect, answerText) => {
+         
+        
         if (isCorrect) {
             setScore(score + 1);
-        }
-                    
+            const updatedArray = [...showCorrectAnswers, answerText];
+            setShowCorrectAnswers(updatedArray);
+        } 
+        
+
         const nextQuestion = currentQuestion + 1;
         if (nextQuestion < questions.length) {
             setCurrentQuestion(nextQuestion);
         } else {
             setShowScore(true);
-        }
+        } 
     }
+        
+
+
+
+    
+    
+    
+    
 
     return (
         <div className='app'>
             {showScore ? (
-                <div className='score-section'>You scored {score} out of {questions.length}. Click on 'Gallery' to return to home page.</div>
+                <div className='score-section'>
+                    <p>You scored {score} out of {questions.length}.</p>
+                <ul>{showCorrectAnswers.map(answer => <li>{answer} ✅</li>)}</ul>
+                    <p>Click on 'Gallery' to return to home page.</p>
+                </div>
             ) : (
                 <>
                     <div className='question-section'>
@@ -81,7 +100,7 @@ const QuizCollection = () => {
                     </div>
                     <div className='answer-section'>
                         {questions[currentQuestion].answerOptions.map((answerOption) => (
-                            <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                            <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect, answerOption.answerText)}>{answerOption.answerText}</button>
                         ))}
                     </div>
                 </>
